64a1f7e62e295b44971e3b623e032444dfdcf6fd
xxx-will <damon__yu@126.com>
2020-07-12 22:36:46 +0800

    modify 21.cpp

--- Week_01_cpp/21.cpp
+++ Week_01_cpp/21.cpp
@@ -1,12 +1,19 @@
+
+struct ListNode 
+{
+    int value;
+    ListNode * next;
+};
+
+
 ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
  
  
- ListNode* h = New(ListNode);
+ ListNode* h = new(ListNode);
  ListNode* cur = h;
  
 
  do {
-
     if (l1->value <= l2->value) {
         //插入q
         cur->next = l1; 
@@ -17,13 +24,13 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
     }
     cur = cur->next;
 
- }while(l1 != null && l2 != null)
+ }while(l1 != nullptr && l2 != nullptr)
 
-if (l1->next != null) {
+if (l1 != nullptr) {
     cur->next = l1;
 }
 
-if (l2->next != null) {
+if (l2 != nullptr) {
     cur->next = l2;
 }
 
